3f4f438a860aeca60e25871c75d71c02309f74b3
theresamay <theresa.may.001@gmail.com>
2016-09-20 21:38:54 -0700

    needed to use as.Date as incoming date fields had alpha month (i.e., Sep).  also needed to adjust the format to ymd.

--- Import_Data.R
+++ Import_Data.R
@@ -15,10 +15,10 @@ sd_req_df <- read.csv("Data/Service_Desk_8-1thru9-20.csv", header=TRUE,
                        sep = ",", stringsAsFactors = FALSE)
 # end
 
-# Convert dates to posix date objects
-sd_req_df$Date.Created <- mdy_hms(sd_req_df$Date.Created)
-sd_req_df$Date.Closed <- mdy_hms(sd_req_df$Date.Closed)
-sd_req_df$Date.Assigned <- mdy_hms(sd_req_df$Date.Assigned)
+### Convert dates to posix date objects
+sd_req_df$New.Created <- ymd(as.Date(sd_req_df$Date.Created, '%B %d %Y'))
+sd_req_df$New.Closed <- ymd(as.Date(sd_req_df$Date.Closed, '%B %d %Y'))
+sd_req_df$New.Assigned <- ymd(as.Date(sd_req_df$Date.Assigned, '%B %d %Y'))
 # end
 
 ##################################
