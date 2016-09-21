9941b3ee1d972bdd6b52809511266854637725b4
theresamay <theresa.may.001@gmail.com>
2016-09-20 20:25:20 -0700

    converted dates to posix date objects - kept hours/minutes/seconds to allow for calculating time to close tickets on same day;  wrote file out for use with other scripts

--- Import_Data.R
+++ Import_Data.R
@@ -11,8 +11,18 @@ library(reshape2)
 library(lubridate)
 
 # assemble the main datafile
-sd_req <- read.csv("Data/Service_Desk_8-1thru9-20.csv", header=TRUE, 
+sd_req_df <- read.csv("Data/Service_Desk_8-1thru9-20.csv", header=TRUE, 
                        sep = ",", stringsAsFactors = FALSE)
 # end
 
-factor(sd_req$Status)
+# Convert dates to posix date objects
+sd_req_df$Date.Created <- mdy_hms(sd_req_df$Date.Created)
+sd_req_df$Date.Closed <- mdy_hms(sd_req_df$Date.Closed)
+sd_req_df$Date.Assigned <- mdy_hms(sd_req_df$Date.Assigned)
+# end
+
+##################################
+# serialize the df
+##################################
+saveRDS(sd_req_df, file="Data/sd_req_df.RDS")
+
