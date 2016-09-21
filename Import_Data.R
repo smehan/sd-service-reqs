db504a4eb51b2f11fb614e41f8c79b518495a924
theresamay <theresa.may.001@gmail.com>
2016-09-20 20:02:23 -0700

    imported csv file - pulled data from SRS at 8:00pm on 9/20/2016.  used existing filter in SRS - Service Desk and SLA Requests between 2 dates.

--- Import_Data.R
+++ Import_Data.R
@@ -11,7 +11,7 @@ library(reshape2)
 library(lubridate)
 
 # assemble the main datafile
-sd_req <- read.csv("Data/SD_Requests_1-1-15_thru_9-30-16.csv", header=TRUE, 
+sd_req <- read.csv("Data/Service_Desk_8-1thru9-20.csv", header=TRUE, 
                        sep = ",", stringsAsFactors = FALSE)
 # end
 
