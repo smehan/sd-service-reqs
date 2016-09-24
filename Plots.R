a90d28d4d586a95951f6d525d7b62410077886c5
theresamay <theresa.may.001@gmail.com>
2016-09-24 15:14:25 -0700

    added comments; made plot pretty

--- Plots.R
+++ Plots.R
@@ -26,7 +26,10 @@ sd_req_2015_df <- subset(sd_req_df, New.Created > '2015-01-01' &
                          New.Created < '2015-12-31')
 sd_req_2016_df <- subset(sd_req_df, New.Created > '2016-01-01')
 sd_req_1516_df <- subset(sd_req_df, New.Created > '2015-01-01')
+### subset after visual management implemented in daily stand-up 
 sd_req_2016_89 <- subset(sd_req_df, New.Created > '2016-08-01')
+### subset after kanban implemented
+sd_req_2016_9 <- subset(sd_req_df, New.Created > '2016-08-29')
 #########
 
 ggplot(sd_req_2016_df) +
@@ -55,16 +58,44 @@ qplot(sd_req_2016_df$tot_days,
       xlab = "SRS Tickets",  
       fill=I("blue"))
 
+####################################################################
+## Plot 2015 and 2016 SRS tickets
+## red lines indicate kanban implementation boundary
+## green lines indicate visual management implementation boundary
+####################################################################
 ggplot() + 
   geom_line(data = sd_req_1516_df, 
             aes(x=New.Created, y=tot_days)) +
-  geom_vline(xintercept=as.numeric(as.Date("2016-08-30")), color = "red") +
+  geom_vline(xintercept=as.numeric(as.Date("2016-08-30")), 
+             color = "red") +
+  geom_text(aes(x=(xintercept=as.Date("2016-08-30")), label = "Kanban Implemented",
+                y=200),
+            color = "red",
+            angle = 90,
+            vjust = 1) +
+  geom_vline(xintercept=as.numeric(as.Date("2016-08-01")), 
+              color = "blue") +
+  geom_text(aes(x=(xintercept=as.Date("2016-08-01")), 
+                label = "Visual Management Implemented",
+                y=200),
+            color = "blue",
+            angle = 90,
+            vjust = 1) +
+  geom_hline(yintercept = as.numeric(max(sd_req_2016_9$tot_days, na.rm=TRUE)),
+              color = "red") +
   geom_hline(yintercept = as.numeric(max(sd_req_2016_89$tot_days, na.rm=TRUE)),  
-                                     color = "green") +
-  ggtitle('Total Service Desk Tickets 2015-2016') +
+              color = "blue") +
+  geom_label(aes(x=as.Date("2015-03-01"),
+                y=300, 
+                label = "Horizontal lines represent \n max days to close"), 
+            color = "azure4",
+            vjust = 1) +
+  ggtitle('Total Days to Close Service Desk Tickets 2015-2016') +
   labs(x="Ticket Create Date",y="Total Days to Close") +
   theme(legend.position='none') 
 
+# TODO
+# add time series script to show 2015 and 2016 overlayed plots
 
 # Need to create a time-series for each variable of concern. Order them by a posix date.
 sd_req_xts <- xts(sd_req_df$tot_days, order.by = sd_req_df$New.Created.Day)
